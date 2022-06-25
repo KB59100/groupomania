c4f6e5cb8326aaf6a717fc8dae74740aae4cb03a
karim bouanani <aude595@yahoo.fr>
2022-06-25 18:40:43 +0200

    Création user et message qui fonctionne

--- back/src/app.js
+++ back/src/app.js
@@ -13,37 +13,74 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get("/users", (req, res) => {
-  const userId = req.params.userId;
 
-  usersData.users.forEach((userId) => {
-    if (userId.id === userId) {
-      userId.push(userId);
-    }
-  });
-    res.status(200).json(userId);
+
+
+//Création du user
+app.post('/users', (req, res) => {
+   try {
+     const body = req.body;
+     usersData.users.push(body);
+     res.status(200).json(usersData);
+   } catch (e) {
+     res.status(500);
+   }
+});
+
+//Récupère tous les users
+app.get('/users', (req, res) => {
+    res.status(200).json(usersData.users);
   }
 );
 
-app.post("/users", (req, res) => {
-  const body = req.body;
+//Récupère un user
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
 
-  usersData.userId.push(body);
-  res.status(200).json(usersData);
-});
- 
-app.delete("/users", (req, res) => {
-  const userId = req.params.userId;
-
-  usersData.users.forEach((userId, index) => {
-    if (userId.id === userId) {
-      usersData.userId.splice(index, 1);
+  usersData.users.forEach((user) => {
+    if (user.id === id) {
+      usersData.users.push(user);
     }
   });
-  res.status(200).json(userId);
+  res.status(200).json(usersData.users);
+});
+
+//Mise a jour d'un user
+app.put('/users/:id', (req, res) => {
+  const body = req.body;
+  const id = req.params.id;
+
+  try{
+  usersData.users.forEach((user) => {
+    if (user.id === id) {
+      user.userName = body.userName;
+      user.email = body.email;
+    }
+  })
+  res.status(200).json(usersData);
+  } catch (e) {
+    res.status(500);
+  }
+
+})
+ 
+//Supprimer un user
+app.delete("/users/:id", (req, res) => {
+  const userId = req.params.id;
+
+  usersData.users.forEach((user, index) => {
+    if (user.id === userId) {
+      usersData.users.splice(index, 1);
+    }
+  });
+  res.status(200).json(usersData.users);
 });
 
 
+
+
+
+//Récupère tous les msg
 app.get('/messages', (req, res) => {
   if(messages.messages.lenght === 0) {
     res.status(404).json({error: "pas de messages"})
@@ -51,57 +88,96 @@ app.get('/messages', (req, res) => {
   res.status(200).json(messages);
 });
 
+//Récupère un message
 app.get("/messages/:id", (req, res) => {
 
   const id = req.params.id;
-  const message = [];
+  const localMessage = [];
 
   messages.messages.forEach((message) => {
     if(message.id === id) {
-      message.push(message);
+      localMessage.push(message);
     }
   })
 
-  if(message.length === 0) {
+  if(localMessage.length === 0) {
     res.status(404).json({error: `le message avec l'id ${id} n'existe pas`});
-
-    res.status(200).json(message[0]);
   }
+  res.status(200).json(localMessage[0]);
 });
 
+//edite un msg
 app.post('/messages', (req, res) => {
   const body = req.body;
 
+  try{
   messages.messages.push(body);
   res.status(200).json(messages);
+  } catch (e) {
+    res.status(500);
+  }
 });
 
+//création du msg
 app.put('/messages/:id', (req, res) => {
   const id = req.params.id;
   const body = req.body;
-  const message = [];
+  const localMessage = [];
 
+  try{
     messages.messages.forEach((message) => {
       if (message.id === id) {
         message.titre = body.titre;
         message.message = body.message;
-        message.push(message);
+        localMessage.push(message);
       }
     })
-    res.status(200).json(message);
+    res.status(200).json(localMessage);
+    } catch (e) {
+      res.status(500);
+    }
 })
 
+//Supprimer un msg
 app.delete('/messages/:id', (req, res) => {
   const id = req.params.id;
-
+      
+  try{
       messages.messages.forEach((message, index) => {
       if (message.id === id) {
         messages.messages.splice(index, 1)
       }
     })
     res.status(200).json(messages);
+    } catch (e) {
+      res.status(500);
+    }
+
 })
 
+
+
+
+//création d'un commentaire associé a un msg
+app.put('/messages/:id/commentaires', (req, res) => {
+  const messageId = req.params.id;
+  const body = req.body;
+  const localCom = [];
+
+  try{
+    messages.messages.forEach((message) => {
+      if (message.id === messageId) {
+        message.commentaires = body.commentaires;
+        localCom.push(message);
+      }
+    })
+    res.status(200).json(localCom);
+    } catch (e) {
+      res.status(500);
+    }
+})
+
+//Edite met a jour un commentaire
 app.post('/messages/:id/commentaires', (req, res) => {
    const messageId = req.params.id;
    const localMessage = [];
@@ -115,23 +191,15 @@ app.post('/messages/:id/commentaires', (req, res) => {
     res.status(200).json(localMessage);
 })
 
+//Récupère tous les commentaires d'un msg
 app.get("/messages/:id/commentaires", (req, res) => {
-  const id = req.params.id;
-  const message = [];
-
-  messages.messages.forEach((message) => {
-    if (message.id === id) {
-      message.push(message);
-    }
-  });
-
-  if (message.length != 0) {
-    res.status(404).json({ error: `le message avec l'id ${id} à bien été ajouté` });
-
-    res.status(200).json(message[0]);
+  if (messages.messages.lenght === 0) {
+    res.status(404).json({ error: "pas de commentaires!" });
   }
+  res.status(200).json(messages);
 });
 
+//Supprime un commentaire d'un msg
 app.delete("/messages/:id/commentaires", (req, res) => {
   const id = req.params.id;
 
@@ -142,6 +210,9 @@ app.delete("/messages/:id/commentaires", (req, res) => {
   });
   res.status(200).json(messages);
 });
+
+
+
 
 
 app.listen(process.env.PORT, () =>
