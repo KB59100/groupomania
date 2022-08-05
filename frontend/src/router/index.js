3fddbf8a072a8a18213cafaa6c8ce58a043991f8
karim bouanani <aude595@yahoo.fr>
2022-08-05 10:32:56 +0200

    modification routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import store from "../store";
 import HomeView from "../views/HomeView.vue";
 import Signup from "../components/Signup.vue";
 import Login from "../components/Login.vue";
@@ -16,32 +17,69 @@ const routes = [
     path: "/signup",
     name: "signup",
     component: Signup,
+    meta: {
+      requiresAuth: false,
+    },
   },
   {
     path: "/login",
     name: "login",
     component: Login,
+    meta: {
+      requiresAuth: false,
+    },
   },
   {
     path: "/view-profile/:id",
     name: "view-profile",
     component: ViewProfile,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/modify-profile/:id",
     name: "modify-profile",
     component: ModifyProfile,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/modify-password/:id",
     name: "modify-password",
     component: ModifyPassword,
+    meta: {
+      requiresAuth: true,
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ["/login", "/signup"];
+  //  const authRequired = !publicPages.includes(to.path);
+
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    if (!store.getters.loggedIn) {
+      next({ name: "login" });
+    } else {
+      next();
+    }
+  } else {
+    if (store.getters.loggedIn) {
+      if (publicPages.includes(to.path)) {
+        next({ name: "home" });
+      }
+    }
+    next(); // does not require auth, make sure to always call next()!
+  }
 });
 
 export default router;
